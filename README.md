045e78d327db7390e7cbe9154c3532223f6962af
Ben Freshwater <ben@frshwtr.uk>
2021-08-12 21:01:05 +0100

    12/08/2021
    * 12-08-2021 Created a fizzbuzz test and script
    
    * 12/08/2021
    
    Co-authored-by: Joost Meijer <joost@betersport.nl>

--- README.md
+++ README.md
@@ -2,16 +2,13 @@
 
 Daily kata practise
 
-## 11/8/2021 Evening
+## 12/8/2021 Evening
 
-So I finally got down to the bottom of my driver issue on Linux, turns out updating just the graphics drivers is a
-no-no. After a lot of googling it turns out I needed to update everything on my system and now it works!
-
-Now I will test-drive Webstorm to see if it performs better with the non-free drivers.
+This evening, I was joined by [joostm020](https://github.com/joostm020) for a little pair programming exercise. We will go through the laws of TDD and solve the FizzBuzz problem, test-driven style!
 
 ### Task
 
-**Time:** 10 mins
+**Time:** As long as we need
 
 #### FizzBuzz
 
@@ -23,14 +20,4 @@ returns FizzBuzz.
 
 ### Wrap up
 
-Today I learned:
-
-* Non-free drivers work much better with Webstorm.
-* `Ctrl+Alt+F10` is not the right command to run tests in Webstorm.
-* `Ctrl+Shift+Bksp` will close all applications and log you out...
-
-Other than the small hickups with commands, I am pleased to see Webstorm performing well with these new drivers. There is less lag making it a much more enjoyable coding experience.
-
-I still need to brush up on my keyboard shortcuts on linux. Perhaps I should standardise the shortcuts across Linux/MacOS?
-
-I think next time I will spice FizzBuzz up and write it in bash or something..
+joostm020 really impressed me. They are pretty new to TDD but picked up the principles very quickly. I hope we get to pair again soon and learn more!
--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,21 +1,34 @@
-const isMultipleOfThree = (number: number): boolean => number % 3 === 0;
+/*
+Write a function that takes a number and outputs them as a string, but for multiples of three returns Fizz
+instead of the number and for the multiples of five returns Buzz. For numbers which are multiples of both three and five
+returns FizzBuzz.
+*/
 
-const isMultipleOfFive = (number: number): boolean => number % 5 === 0;
-
-const isMultipleOfThreeAndFive = (number: number) => isMultipleOfThree(number) && isMultipleOfFive(number);
-
-const fizzBuzz = (number: number) => {
-    if (isMultipleOfThreeAndFive(number)) return 'FizzBuzz';
-    if (isMultipleOfThree(number)) return "Fizz";
-    if (isMultipleOfFive(number)) return "Buzz";
-    return number.toString()
-};
-
-describe('FizzBuzz', () => {
-    describe.each([[1, '1'], [2, '2'], [3, 'Fizz'], [6, 'Fizz'], [4, '4'], [5, 'Buzz'], [10,'Buzz'], [15, 'FizzBuzz'], [30, 'FizzBuzz']])('Given number %s', (input, expectedOutput) => {
+describe('fizzbuzz', () => {
+    describe.each([
+        [1,'1'],
+        [2,'2'],
+        [3,'Fizz'],
+        [4,'4'],
+        [5,'Buzz'],
+        [6,'Fizz'],
+        [15,'FizzBuzz'],
+        [30,'FizzBuzz']
+    ])('given number %s', (input, expectedOutput) => {
         it(`should return ${expectedOutput} as a string`, () => {
-            const result = fizzBuzz(input);
+            const result = fizzbuzz(input);
             expect(result).toBe(expectedOutput);
-        })
+        });
     })
 });
+
+function fizzbuzz(i:number):string {
+    let output = '';
+    if(i % 3 === 0) {
+        output += 'Fizz';
+    }
+    if(i % 5 === 0) {
+        output += 'Buzz';
+    }
+    return output || '' + i;
+}
