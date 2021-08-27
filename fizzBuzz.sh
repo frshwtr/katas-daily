3de7ad1b7177b0348ca85da991b140d670478147
ben.freshwater <ben.freshwater@cazoo.co.uk>
2021-08-27 08:59:47 +0100

    27/8/2021 8:50am

--- fizzBuzz.sh
+++ fizzBuzz.sh
@@ -1,15 +1,34 @@
+#!/bin/bash
+
+function is_multiple_of_three() {
+  if [ $(($1%3)) == 0 ]
+    then
+      return 0
+  fi
+  return 1
+}
+
+function is_multiple_of_five() {
+  if [ $(($1%5)) == 0 ]
+    then
+      return 0
+  fi
+  return 1
+}
+
 out=''
-if [ $(($1 % 3)) == 0 ]
+if is_multiple_of_three "$1"
 then
   out="${out}Fizz"
 fi
-if [ $(($1 % 5)) == 0 ]
+if is_multiple_of_five "$1"
 then
-  out="${out}Buzz"
-fi
-if [[ $out == '' ]]
-then
-  out=$1
+    out="${out}Buzz"
 fi
 
-echo $out
+if [[ $out == '' ]]
+then
+  echo "$1"
+else
+  echo "$out"
+fi
